531d767c2259a819d6d3563d8f1e1cbddea9b406
Ezequiel C. <e.cf@leiuq.fun>
2026-01-06 23:41:03 +0100

    static routes & error page

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { Login } from "@/pages/login"
 import { Hono } from "hono"
-import { getCookie, setCookie } from "hono/cookie"
+import { setCookie } from "hono/cookie"
 import { secureHeaders } from "hono/secure-headers"
 import { exportJWK, generateKeyPair } from "jose"
 import { api } from "@/routes/api"
 import { dashboard } from "@/routes/dashboard"
+import { serveStatic } from "hono/bun"
+import ErrorPage from "./pages/error"
+import { reactRender } from "./routes/common"
 
 const app = new Hono()
 
@@ -34,11 +36,35 @@ app.get("/.well-known/jwks.json", async (c) => {
   return c.json({ keys: [jwk] })
 })
 
+// Static files
+app.use(
+  '/static/*',
+  serveStatic({
+    root: './public',
+    rewriteRequestPath(path) {
+      return path.replace('/static/', '/')
+    },
+    onNotFound(path, c) {
+      console.log(`Static file not found: ${path}`)
+    },
+  }),
+)
+
+// Layout
+app.get(
+  '*',
+  reactRender
+)
 
 // Dashboard routes
 app.route('/dashboard', dashboard)
 
 // API routes
 app.route('/api', api)
+
+// 404 Page
+app.notFound(c => {
+  return c.render(ErrorPage(), { title: "Error!" });
+})
 
 export default app
