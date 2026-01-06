bd2395e611c9119623c9725f935f654388333494
Ezequiel C. <e.cf@leiuq.fun>
2026-01-06 07:45:34 +0100

    changed env name

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import { createClient } from '@libsql/client';
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 
-const client = createClient({ url: process.env.DB_FILE_NAME! });
+const client = createClient({ url: process.env.DATABASE_URL! });
 const db = drizzle({ client });
 
 export { db };
