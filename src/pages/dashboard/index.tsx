9c78f3cf6801247e724b00c8fdb7fb67ce8b936e
Ezequiel C. <e.cf@leiuq.fun>
2026-01-07 00:23:38 +0100

    less load on the page file

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,30 +1,8 @@
-import { DoorClosedLockedIcon, HouseIcon, UserIcon } from "lucide-react"
 
 export default function DashboardIndex() {
   return (
     <>
-      <aside className="fixed h-screen w-16 flex flex-col items-center space-y-4 justify-center shadow-md">
-        <ul className="menu bg-base-200 rounded-box">
-          <li>
-            <a className="tooltip tooltip-right" data-tip="Home" href="/dashboard">
-              <HouseIcon />
-            </a>
-          </li>
-          <li>
-            <a className="tooltip tooltip-right" data-tip="Account" href="/dashboard/account">
-              <UserIcon />
-            </a>
-          </li>
-          <li>
-            <a className="tooltip tooltip-right" data-tip="Admin" href="/dashboard/admin">
-              <DoorClosedLockedIcon />
-            </a>
-          </li>
-        </ul>
-      </aside>
-      <main className="h-full w-full pl-18">
-        <h1 className="text-md font-bold underline">Dashboard Home</h1>
-      </main>
+      <h1 className="text-3xl font-bold">Dashboard</h1>
     </>
   )
 }
